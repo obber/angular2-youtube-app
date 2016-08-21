0c037fbc637ab886c4071d576bb4b98b9e7f154b
Kan Adachi <Kadachi1023@gmail.com>
2016-08-21 11:24:17 -0700

    [setup] initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  moduleId: module.id,
+  template: require('./app.html')
+})
+
+export class App {}
---
+++ src/config.example.js
@@ -0,0 +1,5 @@
+const config = {
+  YOUTUBE_API_KEY: 'YOUTUBE API KEY GOES HERE',
+};
+
+export default config;
